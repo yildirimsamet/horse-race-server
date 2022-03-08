56986b762ab0c7a93152109ff0b59c10f2fa5283
Samet Yildirim <samety3503@gmail.com>
2022-03-08 21:33:16 +0300

    [Feature]Horse model add

---
+++ models/Horse/index.js
@@ -0,0 +1,19 @@
+const db = require('../../config/db');
+class Horse {
+    constructor({ ownerId, name, color, muscleRatio, level, experience, age, weight }) {
+        this.ownerId = ownerId;
+        this.name = name;
+        this.color = color;
+        this.muscleRatio = muscleRatio;
+        this.level = level;
+        this.experience = experience;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    static findUserHorses(userId) {
+        const sql = `SELECT * FROM horse WHERE ownerId = ${userId}`;
+        return db.execute(sql);
+    }
+}
+module.exports = Horse;
